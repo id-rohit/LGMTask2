b5448562d3b537f47ce79c2a5d5c3b4b3d82f1e4
Rohit <rohitrs.8810@gmail.com>
2022-07-11 04:11:29 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,45 @@
-import logo from './logo.svg';
+import React, {Component}from 'react'
 import './App.css';
+import LGM from './LGM.jpg'
+import Users from './components/user';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  constructor(props){
+	super(props)
+	this.state = {users_data :[],
+                loading : true                
+  }
+	this.updateState = this.updateState.bind(this)
+  }
+    
+  updateState(){
+      const link="https://reqres.in/api/users?page=1";
+      fetch(link)
+      .then(response => response.json())
+      .then((users) => {
+        
+        this.setState({users_data :users.data,
+                        loading: false
+        })
+        console.log(users.data);
+      })
+      .catch((error) => {
+        console.error(error)
+      })
+  }
+    
+  render(){
+    return (<>
+        <nav className="nav">
+          <img src={LGM} alt=""></img>
+          <button onClick={this.updateState}>Get Users</button>
+        </nav>
+        <div className="box2">
+         <Users loading={this.state.loading} users={this.state.users_data}/>
+        </div>
+    </>
+    )
+  }
 }
-
+	
 export default App;
